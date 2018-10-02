c3bd57b17d1cdea6d61b5b9db3764f63d764c5c4
rowan <rowan@rowan.website>
2018-10-02 16:01:49 -0400

    Very rudimentary functionality (responds to people it's following, does not follow or RT)

--- str8peoplewatch.py
+++ str8peoplewatch.py
@@ -1,12 +1,23 @@
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 import tweepy
+import json
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+class StreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        if status.user.id in api.friends_ids():
+            id = status.id
+            api.update_status('no', in_reply_to_status_id=id, auto_populate_reply_metadata=True)
+streamListener = StreamListener()
+stream = tweepy.Stream(auth = api.auth, listener=streamListener)
+
+stream.filter(track=['are straight people okay'])
+# tweets = api.home_timeline.search(q="are straight people okay")
+# for tweet in tweets:
+#     # if "are straight people okay" in tweet.text:
+#     print('@'+tweet.user.screen_name + ':', tweet.text)
 
 # api.update_status("no, they're really not")
