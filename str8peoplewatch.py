65b985b7534b9699856a0294238f530ba903a6b3
rowan <rowan@rowan.website>
2018-10-04 13:03:17 -0400

    Move reply section back out of the check-for-mention part

--- str8peoplewatch.py
+++ str8peoplewatch.py
@@ -35,10 +35,10 @@ class StreamListener(tweepy.StreamListener):
 
             # elif (status.user.id in api.followers_ids()):
 
-            if (re.search(r'(\S*r\S*) (str\S*) (p\S*p(l|s)e?) (\S* )?((ok\S*)|(alright))(?mi)', statusText) is None):
-                pass
-            else:
-                api.update_status(random.choice(responses), in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
+        if (re.search(r'(\S*r\S*) (str\S*) (p\S*p(l|s)e?) (\S* )?((ok\S*)|(alright))(?mi)', statusText) is None):
+            pass
+        else:
+            api.update_status(random.choice(responses), in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
 
 streamListener = StreamListener()
 stream = tweepy.Stream(auth = api.auth, listener=streamListener)
