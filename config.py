cf8388d2506f4ad5b069431d6414c509bb1ac5c5
rowan <rowan@rowan.website>
2018-10-03 21:55:57 -0400

    Update logic for regex, wider activation and response pool. Reply to everybody.

--- config.py
+++ config.py
@@ -6,3 +6,30 @@ CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_KEY = os.getenv("ACCESS_KEY")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+HANDLE = 'str8peoplewatch'
+
+listenFor = [
+    '@'+HANDLE,
+    'straight people',
+    'str8 people',
+    'straight ppl',
+    'str8 ppl'
+    'straight folks',
+    'str8 folks'
+]
+noRT = [
+    'no retweet',
+    'no rt',
+    'don\'t retweet',
+    'don\'t rt'
+]
+responses = [
+    'no',
+    'nope',
+    'obviously not',
+    'so not okay',
+    'nooooooooope',
+    'nah',
+    "what the fuck do you think",
+    'oh, honey, if only'
+]
--- str8peoplewatch.py
+++ str8peoplewatch.py
@@ -1,6 +1,7 @@
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+from config import HANDLE, listenFor, noRT, responses
 import tweepy
-import json
+import random, re
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -11,19 +12,31 @@ class StreamListener(tweepy.StreamListener):
         if status_code == 420:
             #returning False in on_error disconnects the stream
             return True
+        else:
+            api.send_direct_message(screen_name=rowanlupton, text="str8peoplewatch is down")
     def on_status(self, status):
-        if any(mentions['screen_name'] == 'str8peoplewatch' for mentions in status.entities['user_mentions']):
-            try:
-                api.retweet(status.id)
-            except: # this should get proper error handling
-                pass
+        if (status.truncated):
+            statusText = status._json['extended_tweet']['full_text']
+        else:
+            statusText = status.text
 
-        elif status.user.id in api.followers_ids():
-            id = status.id
-            api.update_status('no', in_reply_to_status_id=id, auto_populate_reply_metadata=True)
+        if any(mentions['screen_name'] == HANDLE for mentions in status.entities['user_mentions']):
+            # if ('straight' in statusText or 'okay' in statusText):
+            if (re.search(r'((str(8|aight))|(p\S*p(l|s)e?))(?mi)', statusText) is not None):
+                try:
+                    api.retweet(status.id)
+                except: # this should get proper error handling
+                    pass
+
+        # elif (status.user.id in api.followers_ids()):
+
+        if (re.search(r'(\S*r\S*) (str\S*) (p\S*p(l|s)e?) (\S* )?((ok\S*)|(alright))(?mi)', statusText) is None):
+            pass
+        else:
+            api.update_status(random.choice(responses), in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
 
 streamListener = StreamListener()
 stream = tweepy.Stream(auth = api.auth, listener=streamListener)
-stream.filter(track=['are straight people okay', '@str8peoplewatch'], async=True)
+stream.filter(track=listenFor, async=True)
 
 # api.update_status("no, they're really not")
